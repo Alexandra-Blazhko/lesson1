2d608e047aaaab384488e95bf5fe73d44985eddd
zzzfairy <zzzfairy@mail.ru>
2019-02-05 07:51:19 +0300

    first commit

---
+++ lesson1/lesson.js
@@ -0,0 +1,23 @@
+var money = prompt("Ваш бюджет на месяц?", "");
+var time = prompt("Введите дату в формате YYYY-MM-DD", "");
+var appData = {
+    budget: money,
+    timeData: time,
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    savings: false
+};
+for (let i = 0; i < 2; i++) {
+    var a = prompt("Введите обязательную статью расходов в этом месяце", "");
+    var b = prompt("Во сколько обойдётся?", "");
+if ( (typeof(a)) === "string" && (typeof(a)) != null && (typeof(b)) != null
+ && a != "" && b != "" &&  a.length < 50) {
+    console.log("done");
+    appData.expenses[a] = b;
+} else {
+    i = i - 1;
+}
+}
+appData.moneyPerDay = appData.budget / 30;
+alert("Еженедельный бюджет: " + appData.moneyPerDay);
