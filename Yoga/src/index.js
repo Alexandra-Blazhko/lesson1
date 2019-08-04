6c9928ffa03e771509d62163e61bd0d218fea944
zzzfairy <zzzfairy@mail.ru>
2019-08-04 13:58:12 +0300

    Compilate Yoga lesson

---
+++ Yoga/src/index.js
@@ -0,0 +1,19 @@
+window.addEventListener("DOMContentLoaded", function() {
+    "use strict";
+    let tabs = require('./js/parts/tabs.js'),
+        form = require('./js/parts/form.js'),
+        calc = require('./js/parts/calc.js'),
+        slider = require('./js/parts/slider.js'),
+        timer = require('./js/parts/timer.js'),
+        popup = require('./js/parts/popup.js'),
+        modal = require('./js/parts/modal.js');
+
+        tabs();
+        form();
+        calc();
+        slider();
+        timer();
+        modal();
+        popup();
+        
+}); 
